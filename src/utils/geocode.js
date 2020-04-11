b8fc56f3674660d34db205577886de45981f6de9
ChronisG <thegamedevelopergame@gmail.com>
2020-04-11 01:45:17 +0100

    updated .gitignore file, added env variables for api keys

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,20 +1,22 @@
-const request = require('request')
+const request = require("request");
+require("dotenv").config();
+const key = process.env.MAPBOX_KEY;
+const geocode = (address, callback) => {
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/
+    ${encodeURIComponent(address)}.json?access_token=${key}&limit=1`;
+  request({ url, json: true }, (error, response) => {
+    if (error) {
+      callback("Oops! Unable to connect to service, try again later");
+    } else if (response.body.features.length === 0) {
+      callback("Oops! Unable to find location, try another search");
+    } else {
+      callback(undefined, {
+        location: response.body.features[0].place_name,
+        latitude: response.body.features[0].center[1],
+        longitude: response.body.features[0].center[0],
+      });
+    }
+  });
+};
 
-const geocode = (address, callback)=>{
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1IjoiY2hyb25pc2ciLCJhIjoiY2sxbGYyYjNkMDRwdjNjbnNodnZoMWlvdSJ9.BJbXR6KKVU2kLWUgLGBRRw&limit=1'
-    request({url, json:true},(error,response)=>{
-            if(error){
-                    callback('Oops! Unable to connect to service, try again later')
-            }else if(response.body.features.length === 0){
-                    callback('Oops! Unable to find location, try another search')
-            }else{
-                    callback(undefined,{
-                            location: response.body.features[0].place_name,
-                            latitude: response.body.features[0].center[1],
-                            longitude: response.body.features[0].center[0]
-                    })
-            }
-    })
-}
-
-module.exports = geocode
+module.exports = geocode;
